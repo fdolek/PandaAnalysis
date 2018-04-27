b0e3f69ca8934a4e92be34dbcdbac695130a05e0
DianaLeonS <didis@live.com.mx>
2018-04-26 19:13:05 -0500

    Adding LepMonotopSelection

--- LeptonicMonoTop/python/LeptonicMonotopSelection.py
+++ LeptonicMonoTop/python/LeptonicMonotopSelection.py
@@ -1,18 +1,29 @@
 from PandaCore.Tools.Misc import *
 from re import sub
-Data_Trig='( (PD_SingleEle && HLT_EleOR) || (PD_SingleMu && (HLT_MuOR) && !(HLT_EleOR) )) && ( METfilters == 1)'
-IsoVeto='!iso_Veto'
+
+eleTrigger='(trigger&2)!=0'
+muTrigger='(trigger&8)!=0'
+
+presel = 'nTau==0 && Sum$(jetPt>25)>0' #Need to check
 
 cuts = {
-    'bjet0': 'nLep ==1 && Lep_pt > 25 && Selected == 1  &&  nVeto == 0 && nJets30Clean >= 1 && nBJet == 0 && MT>=160 && !iso_Veto',
-    'bjet1': 'nLep ==1 && Lep_pt > 25 && Selected == 1  &&  nVeto == 0 && nJets30Clean >= 1 && nBJet == 1 && MT>=160 && !iso_Veto',
-    'bjet2': 'nLep ==1 && Lep_pt > 25 && Selected == 1  &&  nVeto == 0 && nJets30Clean >= 1 && nBJet == 2 && MT>=160 && !iso_Veto',
-    'nlep2bjetgt0': 'nLep ==2 && Lep_pt > 25 && Selected == 1  &&  nVeto == 0 && nJets30Clean >= 1 && nBJet > 0 && MT>=160 && !iso_Veto',
-}
+    'nLep1': tAND(presel,'nTightLep==1 && (nTightElectron + nTightMuon)==1 && mT>=160'),
+    'nLep2' : tAND(presel,'nTightLep==2 && mT>=160'),
+    }
+
+cuts['wjets'] = tAND(nLep1,'Sum$(jetCSV>0.8 && abs(jetEta)<2.5)==0')
+cuts['signal'] = tAND(nLep1,'Sum$(jetCSV>0.8 && abs(jetEta)<2.5)==1')
+cuts['ttbar1l'] = tAND(nLep1,'Sum$(jetCSV>0.8 && abs(jetEta)<2.5)==2')
+cuts['ttbar2l'] = tAND(nLep2,'Sum$(jetCSV>0.8 && abs(jetEta)<2.5)==2')
+
 
 weights = {
-    'bjet0': 'Xsec*1*btagSF*nISRttweight*puRatio*lepSF',
-    'bjet1': 'Xsec*1*btagSF*nISRttweight*puRatio*lepSF',
-    'bjet2': 'Xsec*1*btagSF*nISRttweight*puRatio*lepSF',
-    'nlep2bjetgt0': 'Xsec*1*btagSF*nISRttweight*puRatio*lepSF',
+    'signal': '%f*sf_pu*sf_tt*normalizedWeight*sf_lepID*sf_lepIso*sf_lepTrack*sf_ewkV*sf_qcdV*sf_qcdTT*sf_btag1',
+    'wjets' : '%f*sf_pu*sf_tt*normalizedWeight*sf_lepID*sf_lepIso*sf_lepTrack*sf_ewkV*sf_qcdV*sf_qcdTT*sf_btag0',
+    'ttbar1l' : '%f*sf_pu*sf_tt*normalizedWeight*sf_lepID*sf_lepIso*sf_lepTrack*sf_ewkV*sf_qcdV*sf_qcdTT*sf_btag2',
+    'ttbar2l': '%f*sf_pu*sf_tt*normalizedWeight*sf_lepID*sf_lepIso*sf_lepTrack*sf_ewkV*sf_qcdV*sf_qcdTT*sf_btag2',
 }
+
+
+
+
