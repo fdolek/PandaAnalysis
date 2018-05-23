d8878995681d86428d037263ff51147c694e6fb2
DianaLeonS <didis@live.com.mx>
2018-05-23 16:44:38 -0500

    fitting fixed

--- LeptonicMonoTop/fitting/makeFittingForest.py
+++ LeptonicMonoTop/fitting/makeFittingForest.py
@@ -18,77 +18,75 @@ import PandaCore.Tools.Functions # kinematics
 import PandaAnalysis.LeptonicMonoTop.LeptonicMonotopSelection as sel
 
 args = parser.parse_args()
-nddt = args.ddt
 region = args.region  
 #out_region = args.region
 #region = out_region.split('_')[0]
 
-for x in ['signale', 'signalm']:
- if region=='test':
-     is_test = True 
-     region = x
- else:
-     is_test = False
+if region=='test':
+    is_test = True 
+    region = 'signal'
+else:
+    is_test = False
 
- argv=[]
+argv=[]
 
- basedir = args.input
- lumi = 35900
+basedir = args.input
+lumi = 35900
 
- def f(x):
-     return basedir + x + '.root'
+def f(x):
+    return basedir + x + '.root'
 
- def shift_btags(additional=None):
-     shifted_weights = {}
-     #if not any([x in region for x in ['signal','top','w']]):
-     #    return shifted_weights 
-     for shift in ['BUp','BDown','MUp','MDown']:
-         for cent in ['sf_btag']:
-             shiftedlabel = ''
-             if 'sj' in cent:
-                 shiftedlabel += 'sj'
-             if 'B' in shift:
-                 shiftedlabel += 'btag'
-             else:
-                 shiftedlabel += 'mistag'
-             if 'Up' in shift:
-                 shiftedlabel += 'Up'
-             else:
-                 shiftedlabel += 'Down'
-             weight = sel.weights[region+'_'+cent+shift]%lumi
-             if additional:
-                 weight = tTIMES(weight,additional)
-             shifted_weights[shiftedlabel] = weight
-     return shifted_weights
+def shift_btags(additional=None):
+    shifted_weights = {}
+    #if not any([x in region for x in ['signal','top','w']]):
+    #    return shifted_weights 
+    for shift in ['BUp','BDown','MUp','MDown']:
+        for cent in ['sf_btag']:
+            shiftedlabel = ''
+            if 'sj' in cent:
+                shiftedlabel += 'sj'
+            if 'B' in shift:
+                shiftedlabel += 'btag'
+            else:
+                shiftedlabel += 'mistag'
+            if 'Up' in shift:
+                shiftedlabel += 'Up'
+            else:
+                shiftedlabel += 'Down'
+            weight = sel.weights[region+'_'+cent+shift]%lumi
+            if additional:
+                weight = tTIMES(weight,additional)
+            shifted_weights[shiftedlabel] = weight
+    return shifted_weights
 
- #vmap definition
- vmap = {}
- vmap['mt'] = 'mT'
+#vmap definition
+vmap = {}
+vmap['mt'] = 'mT'
 
- #weights
- weights = {'nominal' : sel.weights[region]%lumi}
- weights.update(shift_btags())
+#weights
+weights = {'nominal' : sel.weights[region]%lumi}
+weights.update(shift_btags())
 
- factory = forest.RegionFactory(name = region if not(is_test) else 'test',
-                                cut = sel.cuts[region],
-                                variables = vmap, 
-                                mc_weights = weights)
+factory = forest.RegionFactory(name = region if not(is_test) else 'test',
+                               cut = sel.cuts[region],
+                               variables = vmap, 
+                               mc_weights = weights)
 
 #Process and creation of new ntuples process
- if is_test:
-    factory.add_process(f('ZJets'),'Zll')
-    factory.add_process(f('WJets'),'Wlv')
-    factory.add_process(f('SingleTop'),'ST')
-    factory.add_process(f('Diboson'),'Diboson')
-    factory.add_process(f('QCD'),'QCD')
-    factory.add_process(f('TTbar'),'ttbar')
+if is_test:
+   factory.add_process(f('ZJets'),'Zll')
+   factory.add_process(f('WJets'),'Wlv')
+   factory.add_process(f('SingleTop'),'ST')
+   factory.add_process(f('Diboson'),'Diboson')
+   factory.add_process(f('QCD'),'QCD')
+   factory.add_process(f('TTbar'),'ttbar')
 
-     if 'wen' in region or 'ttbar1e' in region or 'ttbar2le' in region or 'signale' in region:
-         factory.add_process(f('SingleElectron'),'Data',is_data=True,extra_cut=sel.eleTrigger)
+    if 'wen' in region or 'ttbar1e' in region or 'ttbar2le' in region or 'signale' in region:
+        factory.add_process(f('SingleElectron'),'Data',is_data=True,extra_cut=sel.eleTrigger)
 
-     if 'wmn' in region or 'ttbar1m' in region or 'ttbar2lm' in region or 'signalm' in region:
-         factory.add_process(f('SingleMuon'),'Data',is_data=True,extra_cut=sel.muTrigger)
+    if 'wmn' in region or 'ttbar1m' in region or 'ttbar2lm' in region or 'signalm' in region:
+        factory.add_process(f('SingleMuon'),'Data',is_data=True,extra_cut=sel.muTrigger)
 
- forestDir = basedir + '/limits/'
- os.system('mkdir -p %s'%(forestDir))
- factory.run(forestDir+'/fittingForest_%s.root'%region)
+forestDir = basedir + '/limits/'
+os.system('mkdir -p %s'%(forestDir))
+factory.run(forestDir+'/fittingForest_%s.root'%region)
