cb397baad28e620242a62c0148da62846fc10269
Sonaina Undleeb <sonaina.undleeb@ttu.edu>
2018-05-29 12:58:50 -0500

    test

--- Flat/test/test.py
+++ Flat/test/test.py
@@ -28,7 +28,6 @@ skimmer = root.PandaAnalyzer(debug_level)
 analysis = boosted(True)
 #analysis = resolved(True)
 #analysis = monojet(True)
-=======
 analysis = monojet(True)
 >>>>>>> b4c5ee56dc683a0af843c730153b5ad7ebf476c2
 skimmer.SetAnalysis(analysis)
