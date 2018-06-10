bf77029daeec7f0ff966f75a0acdf28bdaa4e1bd
DianaLeonS <didis@live.com.mx>
2018-06-10 16:11:23 -0500

    corrected selection

--- LeptonicMonoTop/python/LeptonicMonotopSelection.py
+++ LeptonicMonoTop/python/LeptonicMonotopSelection.py
@@ -16,10 +16,10 @@ cuts['signale']  = tAND(nLep_e,'Sum$(jetCSV>0.8 && abs(jetEta)<2.5)==1')
 cuts['signalm']  = tAND(nLep_m,'Sum$(jetCSV>0.8 && abs(jetEta)<2.5)==1')
 cuts['wen']      = tAND(nLep_e,'Sum$(jetCSV>0.8 && abs(jetEta)<2.5)==0')
 cuts['wmn']      = tAND(nLep_m,'Sum$(jetCSV>0.8 && abs(jetEta)<2.5)==0')
-cuts['ttbar1e']  = tAND(nLep_e,'Sum$(jetCSV>0.8 && abs(jetEta)<2.5)==2')
-cuts['ttbar1m']  = tAND(nLep_m,'Sum$(jetCSV>0.8 && abs(jetEta)<2.5)==2')
-cuts['ttbar2le'] = tAND(nLep2,'nTightMuon==1 && Sum$(jetCSV>0.8 && abs(jetEta)<2.5)==2')
-cuts['ttbar2lm'] = tAND(nLep2,'nTightElectron==1 && Sum$(jetCSV>0.8 && abs(jetEta)<2.5)==2')
+cuts['ten']  = tAND(nLep_e,'Sum$(jetCSV>0.8 && abs(jetEta)<2.5)==2')
+cuts['tmn']  = tAND(nLep_m,'Sum$(jetCSV>0.8 && abs(jetEta)<2.5)==2')
+cuts['tee'] = tAND(nLep2,'nTightElectron==1 && nLooseElectron==2 && Sum$(jetCSV>0.8 && abs(jetEta)<2.5)==2')
+cuts['tmm'] = tAND(nLep2,'nTightMuon==1 && nLooseMuon==2 && Sum$(jetCSV>0.8 && abs(jetEta)<2.5)==2')
 
 weights = {
     'signale'  : '%f*sf_pu*sf_tt*normalizedWeight*sf_lepID*sf_lepIso*sf_lepTrack*sf_ewkV*sf_qcdV*sf_qcdTT*sf_btag1*sf_eleTrig',
@@ -27,19 +27,19 @@ weights = {
     'control'  : '%f*sf_pu*sf_tt*normalizedWeight*sf_lepID*sf_lepIso*sf_lepTrack*sf_ewkV*sf_qcdV*sf_qcdTT',
 }
 
-for x in ['wen','wmn','ttbar1e','ttbar1m','ttbar2le','ttbar2lm']:
-	if 'en' in x or '1e' in x or 'le' in x:
+for x in ['wen','wmn','ten','tmn','tee','tmm']:
+	if 'en' in x or 'ee' in x:
 	  weights[x] = tTIMES(weights['control'],'sf_eleTrig')
 	else:
           weights[x] = tTIMES(weights['control'],'sf_muTrig')
 
-for x in ['wen','wmn','ttbar1e','ttbar1m','ttbar2le','ttbar2lm']:
+for x in ['wen','wmn','ten','tmn','tee','tmm']:
         if 'en' in x or 'mn' in x:
           weights[x] = tTIMES(weights[x],'sf_btag0')
         else: 
           weights[x] = tTIMES(weights[x],'sf_btag2')
 
-for r in ['signale','signalm','wmn','wen','ttbar1e','ttbar1m','ttbar2le','ttbar2lm']:
+for r in ['signale','signalm','wmn','wen','ten','tmn','tee','tmm']:
  for shift in ['BUp','BDown','MUp','MDown']:
   for cent in ['sf_btag']:
    weights[r+'_'+cent+shift] = sub(cent+'0',cent+'0'+shift,sub(cent+'1',cent+'1'+shift,sub(cent+'2',cent+'2'+shift,weights[r])))
