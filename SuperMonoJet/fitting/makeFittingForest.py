8654b0825c755ac192d642545acec2617a8ad96a
Matteo Cremonesi <matteo.cremonesi@cern.ch>
2018-08-30 14:05:14 -0500

    fixing names in the fitting forest

--- SuperMonoJet/fitting/makeFittingForest.py
+++ SuperMonoJet/fitting/makeFittingForest.py
@@ -184,13 +184,13 @@ else:
     factory.add_process(f('Diboson'),'Diboson')
     factory.add_process(f('TTbar'),'ttbar')
 
-    if 'zee' in region or 'te' in region or 'wen' in region:
+    if 'zee' in region or 'en' in region:
         factory.add_process(f('SingleElectron'),'Data',is_data=True,extra_cut=sel.eleTrigger)
 
-    if 'zmm' in region or 'tm' in region or 'wmn' in region:
+    if 'zmm' in region or 'mn' in region:
         factory.add_process(f('MET'),'Data',is_data=True,extra_cut=sel.metTrigger)
     
-    if 'signal' in region or 'te' in region or 'wen' in region or 'tm' in region or 'wmn' in region:
+    if 'signal' in region or 'en' in region or 'mn' in region:
         factory.add_process(f('WJets'),'Wlv')
         factory.add_process(f('SingleTop'),'ST')
         factory.add_process(f('QCD'),'QCD')
