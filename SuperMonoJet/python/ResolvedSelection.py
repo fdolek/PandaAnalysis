97edaf2b2564afd4ff43c038e947686a7da2a3b7
Matteo Cremonesi <matteo.cremonesi@cern.ch>
2018-10-03 15:11:09 -0500

    fixing resolved

--- SuperMonoJet/python/ResolvedSelection.py
+++ SuperMonoJet/python/ResolvedSelection.py
@@ -25,26 +25,26 @@ cuts['pho'] = tAND(baseline,'(nLooseMuon+nLooseElectron+nTau)==0 && nLoosePhoton
 
 for r in ['signal','tmn','ten','zmm','zee','wen','wmn','pho']:
 #	cuts[r] = tAND(cuts[r],'(tanh(atanh((2*jetCSV[bosonjtidx[0]])-1)+atanh((2*jetCSV[bosonjtidx[1]])-1))+1)/2>0.48') 
-        cuts[r] = tAND(cuts[r],'(jetCSV[bosonjtidx[0]]>0.8484 && jetEta[bosonjtidx[0]]<2.4)||(jetCSV[bosonjtidx[1]]>0.8484 && jetEta[bosonjtidx[1]]<2.4)')
         cuts[r+'_fail'] = tAND(cuts[r],'!(jetCSV[bosonjtidx[0]]>0.8484 && jetEta[bosonjtidx[0]]<2.4)||(jetCSV[bosonjtidx[1]]>0.8484 && jetEta[bosonjtidx[1]]<2.4)')
+        cuts[r] = tAND(cuts[r],'(jetCSV[bosonjtidx[0]]>0.8484 && jetEta[bosonjtidx[0]]<2.4)||(jetCSV[bosonjtidx[1]]>0.8484 && jetEta[bosonjtidx[1]]<2.4)')
 
 for r in ['signal','zmm','zee','wen','wmn','pho','signal_fail','wmn_fail','wen_fail','zmm_fail','zee_fail','pho','pho_fail']:
 	if 'fail' in r:
           cuts[r] = tAND(cuts[r],'Sum$(jetCSV>0.8484 && jetEta<2.4)==0')
 	else:
-          cuts[r] = tAND(cuts[r],
-                         '(((jetCSV[bosonjtidx[0]]>0.8484 && jetEta[bosonjtidx[0]]<2.4)&&!(jetCSV[bosonjtidx[1]]>0.8484 && jetEta[bosonjtidx[1]]<2.4) && Sum$(jetCSV>0.8484 && jetEta<2.4)==1) ||
-                         (!(jetCSV[bosonjtidx[0]]>0.8484 && jetEta[bosonjtidx[0]]<2.4)&&(jetCSV[bosonjtidx[1]]>0.8484 && jetEta[bosonjtidx[1]]<2.4) && Sum$(jetCSV>0.8484 && jetEta<2.4)==1) ||
-                         ((jetCSV[bosonjtidx[0]]>0.8484 && jetEta[bosonjtidx[0]]<2.4)&&(jetCSV[bosonjtidx[1]]>0.8484 && jetEta[bosonjtidx[1]]<2.4) && Sum$(jetCSV>0.8484 && jetEta<2.4)==2))')
+          cuts[r] = tAND(cuts[r],'Sum$(jetCSV>0.8484 && jetEta<2.4)<3')
+#                         '(((jetCSV[bosonjtidx[0]]>0.8484 && jetEta[bosonjtidx[0]]<2.4)&&!(jetCSV[bosonjtidx[1]]>0.8484 && jetEta[bosonjtidx[1]]<2.4) && Sum$(jetCSV>0.8484 && jetEta<2.4)==1) ||
+#                         (!(jetCSV[bosonjtidx[0]]>0.8484 && jetEta[bosonjtidx[0]]<2.4)&&(jetCSV[bosonjtidx[1]]>0.8484 && jetEta[bosonjtidx[1]]<2.4) && Sum$(jetCSV>0.8484 && jetEta<2.4)==1) ||
+#                         ((jetCSV[bosonjtidx[0]]>0.8484 && jetEta[bosonjtidx[0]]<2.4)&&(jetCSV[bosonjtidx[1]]>0.8484 && jetEta[bosonjtidx[1]]<2.4) && Sum$(jetCSV>0.8484 && jetEta<2.4)==2))')
 
 for r in ['tmn','ten','tmn_fail','ten_fail']:
 	if 'fail' in r:
           cuts[r] = tAND(cuts[r],'Sum$(jetCSV>0.8484 && jetEta<2.4)>0')
 	else:
-          cuts[r] = tAND(cuts[r],
-                         '(((jetCSV[bosonjtidx[0]]>0.8484 && jetEta[bosonjtidx[0]]<2.4)&&!(jetCSV[bosonjtidx[1]]>0.8484 && jetEta[bosonjtidx[1]]<2.4) && Sum$(jetCSV>0.8484 && jetEta<2.4)>1) ||
-                         (!(jetCSV[bosonjtidx[0]]>0.8484 && jetEta[bosonjtidx[0]]<2.4)&&(jetCSV[bosonjtidx[1]]>0.8484 && jetEta[bosonjtidx[1]]<2.4) && Sum$(jetCSV>0.8484 && jetEta<2.4)>1) ||
-                         ((jetCSV[bosonjtidx[0]]>0.8484 && jetEta[bosonjtidx[0]]<2.4)&&(jetCSV[bosonjtidx[1]]>0.8484 && jetEta[bosonjtidx[1]]<2.4) && Sum$(jetCSV>0.8484 && jetEta<2.4)>2))')
+          cuts[r] = tAND(cuts[r],'Sum$(jetCSV>0.8484 && jetEta<2.4)>2')
+#                         '(((jetCSV[bosonjtidx[0]]>0.8484 && jetEta[bosonjtidx[0]]<2.4)&&!(jetCSV[bosonjtidx[1]]>0.8484 && jetEta[bosonjtidx[1]]<2.4) && Sum$(jetCSV>0.8484 && jetEta<2.4)>1) ||
+#                         (!(jetCSV[bosonjtidx[0]]>0.8484 && jetEta[bosonjtidx[0]]<2.4)&&(jetCSV[bosonjtidx[1]]>0.8484 && jetEta[bosonjtidx[1]]<2.4) && Sum$(jetCSV>0.8484 && jetEta<2.4)>1) ||
+#                         ((jetCSV[bosonjtidx[0]]>0.8484 && jetEta[bosonjtidx[0]]<2.4)&&(jetCSV[bosonjtidx[1]]>0.8484 && jetEta[bosonjtidx[1]]<2.4) && Sum$(jetCSV>0.8484 && jetEta<2.4)>2))')
 
 #weights for specific regions
 weights = {
@@ -75,13 +75,13 @@ for x in ['signal','signal_fail','wmn','wen','zmm','zee','wmn_fail','wen_fail','
 	if 'fail' in x:
 	  weights[x] = tTIMES(weights[x],'sf_btag0')
         else:
-	  weights[x] = tTIMES(weights[x],'sf_btag1')
+	  weights[x] = tTIMES(weights[x],'sf_btag2')
    
 for x in ['tmn','ten','tmn_fail','ten_fail']:
 	if 'fail' in x:
 	  weights[x] = tTIMES(weights[x],'sf_btag1')
         else:
-	  weights[x] = tTIMES(weights[x],'sf_btag2')
+	  weights[x] = tTIMES(weights[x],'sf_btag3')
 
 
 for r in ['signal','tmn','ten','wmn','wen','zmm','zee','signal_fail','tmn_fail','ten_fail','wmn_fail','wen_fail','zmm_fail','zee_fail','pho','pho_fail']:
